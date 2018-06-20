1996a86f0893c8dd59aa6843d847b9063cec5894
sturtuk <edwinsturt@hotmail.co.uk>
2018-06-20 11:59:21 +0530

    Bugsnag Config

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@
 
 // Env vars should be casted to correct types
 const config = {
-  PORT: Number(process.env.PORT) || 9000,
-  NODE_ENV: process.env.NODE_ENV,
-  LOG_LEVEL: process.env.LOG_LEVEL,
-  ALLOW_HTTP: process.env.ALLOW_HTTP === 'true',
-  DEBUG_MODE: process.env.DEBUG_MODE === 'true',
+  PORT: 9000,
+  NODE_ENV: 'development',
+  LOG_LEVEL: 'console',
+  ALLOW_HTTP: 'true',
+  DEBUG_MODE: 'true',
   API_TOKENS: [],
 };
 
