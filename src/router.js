e7d5e833bbfaaaaeac595d66d46392a78d389d5c
vinod <vinod@maximizer-e-services.com>
2018-10-18 14:47:44 +0530

    Debug update

--- src/router.js
+++ src/router.js
@@ -29,7 +29,7 @@ function createRouter() {
   const getRenderSchema = {
     query: renderQuerySchema,
     options: {
-      allowUnknownBody: false,
+      allowUnknownBody: true,
       allowUnknownQuery: true,
     },
   };
